32096b9d74ff0ea8ab0a13ed5f38bcf5d66ff941
Inaaya Omardee <inaayaomardeen@usf.edu>
2025-11-23 00:50:17 -0400

    Add final encouragement message for user

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ print("Welcome to my Python program!")
 
 #Ask user for their input
 hours = input("How many hours did you study today?")
+#Perform a calculation (weekly study estimate)
+weekly_hours = hours * 6
+
+#Display the result clearly
+print(f"You are on track to study approximately {weekly_hours} hours this week.")
 
 #Convert input safely with error handling 
 try:
@@ -10,12 +15,6 @@ try:
 except ValueError:
     print("Please enter a valid number for hours.")
     exit()
-
-#Perform a calculation (weekly study estimate)
-weekly_hours = hours * 7
-
-#Display the result clearly
-print(f"You are on track to study approximately {weekly_hours} hours this week.")
 
 #Encourage the user
 print("Keep up the awesome work!")
